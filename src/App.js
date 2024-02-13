367c53c22eab91578803f6a78b6491fb24b9c174
Kirsty <kirsty02last@gmail.com>
2024-02-13 19:48:46 +0000

    ui for portfolio page done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import ViewProject from './components/ViewProject'; 
 import './styles/style.scss'; 
 
 function App() {
+  const [portfolioItems, setPortfolioItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/portfolioData.json');
+        const data = await response.json();
+        setPortfolioItems(data);
+      } catch (error) {
+        console.error('Error fetching portfolio data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio" element={<Portfolio portfolioItems={portfolioItems} />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/project/:projectId" element={<ViewProject portfolioItems={portfolioItems} />} />
         </Routes>
       </Layout>
     </BrowserRouter>
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const PortfolioItem = ({ title, description, image }) => {
+import { Link } from 'react-router-dom';
+
+const PortfolioItem = ({ title, description, imagePreview, id }) => {
     return (
-        <div className="portfolio-item">
-            <img src={image} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
+        <div className="portfolio-item about ">
+            <img src={imagePreview} alt={title} />
+            <div className='aboutContainer'>
+                <hr></hr>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <Link to={`/project/${id}`}> 
+                    <button className="secondary-btn">VIEW PROJECT</button>
+                </Link>
+                <hr></hr>
+            </div>
         </div>
+        
     );
 };
 
